fc19ac7d4ad4e5fa4fa51c4c2127be61d28f7974
metatypeman <metatypeman@gmail.com>
2016-05-30 11:17:15 +0300

    I have moved files into SquaresWorkBench.TypicalVisualComponents from other inteкnal project.

---
+++ SquaresWorkBench.TypicalVisualComponents/BadHuman.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquaresWorkBench.TypicalVisualComponents
+{
+    public class BadHuman : BaseHuman
+    {
+        public BadHuman()
+        {
+            CurrentBrush = System.Windows.Media.Brushes.Red;
+        }
+
+        protected override void OnSetAlive()
+        {
+            if (Alive)
+            {
+                CurrentBrush = System.Windows.Media.Brushes.Red;
+            }
+            else
+            {
+                CurrentBrush = System.Windows.Media.Brushes.Black;
+            }
+        }
+    }
+}
---
+++ SquaresWorkBench.TypicalVisualComponents/PlaceEntity.cs
@@ -0,0 +1,22 @@
+﻿using SquaresWorkBench.CommonEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace SquaresWorkBench.TypicalVisualComponents
+{
+    public class PlaceEntity : BaseEntity
+    {
+        protected override void OnCreateGeometry()
+        {
+            var rect = new Rect(new Point(0, 0), new Size(150, 150));
+            var tmpRectG = new RectangleGeometry(rect);
+
+            CurrentGeometry = tmpRectG;
+        }
+    }
+}
