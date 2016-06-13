00a49f3bee64459e5350096b93cb2ce11fd315a9
metatypeman <metatypeman@gmail.com>
2016-06-13 09:36:16 +0300

    I have added project GnuClay.Engine. There will be my simple AI-logic.

--- GnuClay.Engine.Tests/Properties/AssemblyInfo.cs
+++ GnuClay.Engine.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GnuClay.LogicalCore.Test")]
+[assembly: AssemblyTitle("GnuClay.Engine.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GnuClay.LogicalCore.Test")]
+[assembly: AssemblyProduct("GnuClay.Engine.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e07f7737-7a25-46a6-b332-404bacd327cb")]
+[assembly: Guid("bf229227-e488-4f28-b2d3-855056369834")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
--- GnuClay.Engine/Properties/AssemblyInfo.cs
+++ GnuClay.Engine/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GnuClay.LogicalCore")]
+[assembly: AssemblyTitle("GnuClay.Engine")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GnuClay.LogicalCore")]
+[assembly: AssemblyProduct("GnuClay.Engine")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1d30b393-cc1c-4c59-8fb9-014199c5eb92")]
+[assembly: Guid("06fda9c5-43c7-49d0-98ad-5b02a96d08c3")]
 
 // Version information for an assembly consists of the following four values:
 //
