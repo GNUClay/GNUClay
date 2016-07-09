20c2c8167156a33a3a43438ab9f09320316d4d4b
metatypeman <metatypeman@gmail.com>
2016-07-09 14:54:53 +0300

    Small snaopshot. I began parsing of conceptual node name. I will be do it by finite-state machines.

---
+++ GnuClay.Engine/CGResolver/Implementations/FromECGToICG/NodeNameLexer/NodeNameToken.cs
@@ -0,0 +1,62 @@
+﻿using GnuClay.CommonUtils.TypeHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GnuClay.Engine.CGResolver.Implementations.FromECGToICG.NodeNameLexer
+{
+    public enum NodeNameTokenKind
+    {
+        Undefined,
+        String,
+
+        /// <summary>
+        /// ∃
+        /// </summary>
+        ExistentialQuantifier,
+
+        /// <summary>
+        /// ∀ or *
+        /// </summary>
+        UniversalQuantifier,
+
+        /// <summary>
+        /// ?
+        /// </summary>
+        Question,
+
+        /// <summary>
+        /// #
+        /// </summary>
+        Octothorpe
+    }
+
+    public class NodeNameToken : IToStringData
+    {
+        public NodeNameTokenKind Kind = NodeNameTokenKind.Undefined;
+
+        public string Content = string.Empty;
+
+        public override string ToString()
+        {
+            return _ObjectHelper.PrintDefaultToStringInformation(this);
+        }
+
+        public string ToStringData()
+        {
+            var tmpSb = new StringBuilder();
+
+            tmpSb.Append(nameof(Kind));
+            tmpSb.Append("=");
+            tmpSb.AppendLine(Kind.ToString());
+
+            tmpSb.Append(nameof(Content));
+            tmpSb.Append("=");
+            tmpSb.AppendLine(Content);
+
+            return tmpSb.ToString();
+        }
+    }
+}
