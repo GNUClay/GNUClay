31b8364b7300733f630e697ae0f1b10f23062d24
Sergey Tolkachev <metatypeman@gmail.com>
2017-02-21 21:34:39 +0200

    fix

--- TSTConsoleWorkBench/TextCGParser/TextCGParserRunner.cs
+++ TSTConsoleWorkBench/TextCGParser/TextCGParserRunner.cs
@@ -77,5 +77,57 @@ namespace TSTConsoleWorkBench.TextCGParser
             queryStr = "INSERT {>: {`part of speech`(man, noun)}}";
             engine.Query(queryStr);
         }
+
+        private const string NP = "NP";
+        private const string QW = "QW";
+        private const string V = "V";
+        private const string to = "to";
+        private const string not = "not";
+        private const string have = "have";
+        private const string will = "will";
+        private const string must = "must";
+        private const string can = "can";
+        private const string _do = "do";
+        private const string may = "may";
+        private const string could = "could";
+        private const string might = "might";
+        private const string be = "be";
+        private const string abble = "able";
+        private const string F_to_do = "(f to do)";
+        private const string F_to_be = "(f to be)";
+        private const string F_to_have = "(f to have)";
+
+        private class N
+        {
+            public string N_1 = string.Empty;
+            public string N_2 = string.Empty;
+            public string N_3 = string.Empty;
+            public string N_4 = string.Empty;
+            public string N_5 = string.Empty;
+            public string N_6 = string.Empty;
+            public string N_7 = string.Empty;
+            public string N_8 = string.Empty;
+        }
+
+        public void GetarateATNStateTree()
+        {
+            var nodes = new List<N>()
+            {
+                new N()
+                {
+                    N_1 = NP,
+                    N_2 = V
+                },
+                new N ()
+                {
+                    N_1 = NP,
+                    N_2 = F_to_do,
+                    N_3 = not,
+                    N_4 = V
+                }
+            };
+
+
+        }
     }
 }
