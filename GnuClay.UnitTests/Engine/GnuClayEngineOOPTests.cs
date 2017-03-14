a7248dc5ddbc11e4d03dc12e67b0ed6389fbaa03
Sergey Tolkachev <metatypeman@gmail.com>
2017-03-14 21:52:46 +0200

    snapshot

--- GnuClay.UnitTests/Engine/GnuClayEngineOOPTests.cs
+++ GnuClay.UnitTests/Engine/GnuClayEngineOOPTests.cs
@@ -97,6 +97,9 @@ namespace GnuClay.UnitTests.Engine
             Assert.AreEqual(qr_3.Success, true);
             Assert.AreEqual(qr_3.HaveBeenFound, false);
 
+            var numberKey = tmpEngine.DataDictionary.GetKey("number");
+            var xVarKey = tmpEngine.DataDictionary.GetKey("$X");
+
             queryString = "SELECT { >:{`count of feet`(#957B6203_D200_47E0_B51E_0E8DEF869B3D,$X)}}";
             var qr_4 = tmpEngine.Query(queryString);
 
@@ -114,12 +117,6 @@ namespace GnuClay.UnitTests.Engine
 
             Assert.AreEqual(targetVarItem.Kind, ExpressionNodeKind.Value);
             Assert.AreEqual(targetVarItem.Value, 2);
-            Assert.AreNotEqual(targetVarItem.EntityKey, 0);
-            Assert.AreNotEqual(targetVarItem.ParamKey, 0);
-
-            var numberKey = tmpEngine.DataDictionary.GetKey("number");
-            var xVarKey = tmpEngine.DataDictionary.GetKey("$X");
-
             Assert.AreEqual(targetVarItem.EntityKey, numberKey);
             Assert.AreEqual(targetVarItem.ParamKey, xVarKey);
 
@@ -132,6 +129,10 @@ namespace GnuClay.UnitTests.Engine
 
             Assert.AreEqual(tmpSelectResult.Items.Count, 0);
 
+            var tKey = tmpEngine.DataDictionary.GetKey("⊤");
+            var humanKey = tmpEngine.DataDictionary.GetKey("human");
+            var bipedKey = tmpEngine.DataDictionary.GetKey("biped");
+
             queryString = "SELECT {>: {is(#957B6203_D200_47E0_B51E_0E8DEF869B3D,$X)}}";
             tmpSelectResult = tmpEngine.Query(queryString);
 
@@ -142,44 +143,118 @@ namespace GnuClay.UnitTests.Engine
             Assert.AreEqual(tmpSelectResult.Items.Count, 3);
 
             targetItem = tmpSelectResult.Items[0];
-
             Assert.AreEqual(targetItem.Params.Count, 1);
-
             targetVarItem = targetItem.Params.Single();
+
+            Assert.AreEqual(targetVarItem.Kind, ExpressionNodeKind.Entity);
+            Assert.AreEqual(targetVarItem.Value, null);
+            Assert.AreEqual(targetVarItem.EntityKey, humanKey);
+            Assert.AreEqual(targetVarItem.ParamKey, xVarKey);
 
             targetItem = tmpSelectResult.Items[1];
-
             Assert.AreEqual(targetItem.Params.Count, 1);
-
             targetVarItem = targetItem.Params.Single();
+
+            Assert.AreEqual(targetVarItem.Kind, ExpressionNodeKind.Entity);
+            Assert.AreEqual(targetVarItem.Value, null);
+            Assert.AreEqual(targetVarItem.EntityKey, tKey);
+            Assert.AreEqual(targetVarItem.ParamKey, xVarKey);
 
             targetItem = tmpSelectResult.Items[2];
-
             Assert.AreEqual(targetItem.Params.Count, 1);
-
             targetVarItem = targetItem.Params.Single();
 
-            NLog.LogManager.GetCurrentClassLogger().Info($"queryString (2) = `{queryString}`");
-            NLog.LogManager.GetCurrentClassLogger().Info($"tmpSelectResult = {tmpSelectResult.ToJson()}");
-            NLog.LogManager.GetCurrentClassLogger().Info($"tmpSelectResult = {SelectResultDebugHelper.ConvertToString(tmpSelectResult, tmpEngine.DataDictionary)}");
-            NLog.LogManager.GetCurrentClassLogger().Info($"{nameof(tmpSelectResult)} = {tmpSelectResult}");
+            Assert.AreEqual(targetVarItem.Kind, ExpressionNodeKind.Entity);
+            Assert.AreEqual(targetVarItem.Value, null);
+            Assert.AreEqual(targetVarItem.EntityKey, bipedKey);
+            Assert.AreEqual(targetVarItem.ParamKey, xVarKey);
+
+            var robotKey = tmpEngine.DataDictionary.GetKey("robot");
+            var instanceKey = tmpEngine.DataDictionary.GetKey("#957b6203_d200_47e0_b51e_0e8def869b3d");
 
             queryString = "SELECT {>: {is($X ,biped)}}";
             tmpSelectResult = tmpEngine.Query(queryString);
-            NLog.LogManager.GetCurrentClassLogger().Info($"queryString (2) = `{queryString}`");
-            NLog.LogManager.GetCurrentClassLogger().Info($"tmpSelectResult = {tmpSelectResult.ToJson()}");
-            NLog.LogManager.GetCurrentClassLogger().Info($"tmpSelectResult = {SelectResultDebugHelper.ConvertToString(tmpSelectResult, tmpEngine.DataDictionary)}");
-            NLog.LogManager.GetCurrentClassLogger().Info($"{nameof(tmpSelectResult)} = {tmpSelectResult}");
+
+            Assert.AreNotEqual(tmpSelectResult, null);
+            Assert.AreEqual(tmpSelectResult.Success, true);
+            Assert.AreEqual(tmpSelectResult.HaveBeenFound, true);
+
+            Assert.AreEqual(tmpSelectResult.Items.Count, 3);
+
+            targetItem = tmpSelectResult.Items[0];
+            Assert.AreEqual(targetItem.Params.Count, 1);
+            targetVarItem = targetItem.Params.Single();
+
+            Assert.AreEqual(targetVarItem.Kind, ExpressionNodeKind.Entity);
+            Assert.AreEqual(targetVarItem.Value, null);
+            Assert.AreEqual(targetVarItem.EntityKey, robotKey);
+            Assert.AreEqual(targetVarItem.ParamKey, xVarKey);
+
+            targetItem = tmpSelectResult.Items[1];
+            Assert.AreEqual(targetItem.Params.Count, 1);
+            targetVarItem = targetItem.Params.Single();
+
+            Assert.AreEqual(targetVarItem.Kind, ExpressionNodeKind.Entity);
+            Assert.AreEqual(targetVarItem.Value, null);
+            Assert.AreEqual(targetVarItem.EntityKey, humanKey);
+            Assert.AreEqual(targetVarItem.ParamKey, xVarKey);
+
+            targetItem = tmpSelectResult.Items[2];
+            Assert.AreEqual(targetItem.Params.Count, 1);
+            targetVarItem = targetItem.Params.Single();
+
+            Assert.AreEqual(targetVarItem.Kind, ExpressionNodeKind.Entity);
+            Assert.AreEqual(targetVarItem.Value, null);
+            Assert.AreEqual(targetVarItem.EntityKey, instanceKey);
+            Assert.AreEqual(targetVarItem.ParamKey, xVarKey);
+
 
             queryString = "SELECT {>: {biped($X)}}";
             tmpSelectResult = tmpEngine.Query(queryString);
-            NLog.LogManager.GetCurrentClassLogger().Info($"queryString (2) = `{queryString}`");
-            NLog.LogManager.GetCurrentClassLogger().Info($"tmpSelectResult = {tmpSelectResult.ToJson()}");
-            NLog.LogManager.GetCurrentClassLogger().Info($"tmpSelectResult = {SelectResultDebugHelper.ConvertToString(tmpSelectResult, tmpEngine.DataDictionary)}");
-            NLog.LogManager.GetCurrentClassLogger().Info($"{nameof(tmpSelectResult)} = {tmpSelectResult}");
+
+            Assert.AreNotEqual(tmpSelectResult, null);
+            Assert.AreEqual(tmpSelectResult.Success, true);
+            Assert.AreEqual(tmpSelectResult.HaveBeenFound, true);
+
+            Assert.AreEqual(tmpSelectResult.Items.Count, 3);
+
+            targetItem = tmpSelectResult.Items[0];
+            Assert.AreEqual(targetItem.Params.Count, 1);
+            targetVarItem = targetItem.Params.Single();
+
+            Assert.AreEqual(targetVarItem.Kind, ExpressionNodeKind.Entity);
+            Assert.AreEqual(targetVarItem.Value, null);
+            Assert.AreEqual(targetVarItem.EntityKey, robotKey);
+            Assert.AreEqual(targetVarItem.ParamKey, xVarKey);
+
+            targetItem = tmpSelectResult.Items[1];
+            Assert.AreEqual(targetItem.Params.Count, 1);
+            targetVarItem = targetItem.Params.Single();
+
+            Assert.AreEqual(targetVarItem.Kind, ExpressionNodeKind.Entity);
+            Assert.AreEqual(targetVarItem.Value, null);
+            Assert.AreEqual(targetVarItem.EntityKey, humanKey);
+            Assert.AreEqual(targetVarItem.ParamKey, xVarKey);
+
+            targetItem = tmpSelectResult.Items[2];
+            Assert.AreEqual(targetItem.Params.Count, 1);
+            targetVarItem = targetItem.Params.Single();
+
+            Assert.AreEqual(targetVarItem.Kind, ExpressionNodeKind.Entity);
+            Assert.AreEqual(targetVarItem.Value, null);
+            Assert.AreEqual(targetVarItem.EntityKey, instanceKey);
+            Assert.AreEqual(targetVarItem.ParamKey, xVarKey);
+
 
             queryString = "SELECT {>: {is($X ,$Y)}}";
             tmpSelectResult = tmpEngine.Query(queryString);
+
+            Assert.AreNotEqual(tmpSelectResult, null);
+            Assert.AreEqual(tmpSelectResult.Success, true);
+            Assert.AreEqual(tmpSelectResult.HaveBeenFound, true);
+
+            Assert.AreEqual(tmpSelectResult.Items.Count, 11);
+
             NLog.LogManager.GetCurrentClassLogger().Info($"queryString (2) = `{queryString}`");
             NLog.LogManager.GetCurrentClassLogger().Info($"tmpSelectResult = {tmpSelectResult.ToJson()}");
             NLog.LogManager.GetCurrentClassLogger().Info($"tmpSelectResult = {SelectResultDebugHelper.ConvertToString(tmpSelectResult, tmpEngine.DataDictionary)}");
