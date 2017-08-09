4f4dff6d96967949035dc2b8a11c577ca13fedae
Sergiy Tolkachov <metatypeman@gmail.com>
2017-08-09 16:59:51 +0300

    snapshot

--- GnuClay.Engine/LogicalStorage/InternalStorage/InternalStorageEngine.cs
+++ GnuClay.Engine/LogicalStorage/InternalStorage/InternalStorageEngine.cs
@@ -16,6 +16,7 @@ namespace GnuClay.Engine.LogicalStorage.InternalStorage
         }
 
         public Dictionary<ulong, List<RulePart>> mRelationPartsIndex;
+        public Dictionary<RulePart, List<ulong>> mRelationPartsDict;
         public Dictionary<ulong, RuleInstance> mRulesAndFactsDict;
         public Dictionary<ulong, List<RuleInstance>> mLongHasheCodeRulesAndFactsDict;
         public List<ulong> mEntitiesList;
@@ -24,6 +25,7 @@ namespace GnuClay.Engine.LogicalStorage.InternalStorage
         private class Data
         {
             public Dictionary<ulong, List<RulePart>> mRelationPartsIndex;
+            public Dictionary<RulePart, List<ulong>> mRelationPartsDict;
             public Dictionary<ulong, RuleInstance> mRulesAndFactsDict;
             public Dictionary<ulong, List<RuleInstance>> mLongHasheCodeRulesAndFactsDict;
             public List<ulong> mEntitiesList;
@@ -40,16 +42,30 @@ namespace GnuClay.Engine.LogicalStorage.InternalStorage
             else
             {
                 tmpList = new List<RulePart>();
-
                 mRelationPartsIndex.Add(key, tmpList);
             }
 
-            if(tmpList.Contains(rulePart))
+            if(!tmpList.Contains(rulePart))
             {
-                return;
+                tmpList.Add(rulePart);
             }
 
-            tmpList.Add(rulePart);
+            List<ulong> tmpKeysList = null;
+
+            if(mRelationPartsDict.ContainsKey(rulePart))
+            {
+                tmpKeysList = mRelationPartsDict[rulePart];
+            }
+            else
+            {
+                tmpKeysList = new List<ulong>();
+                mRelationPartsDict.Add(rulePart, tmpKeysList);
+            }
+
+            if(!tmpKeysList.Contains(key))
+            {
+                tmpKeysList.Add(key);
+            }
         }
 
         public List<RulePart> GetIndex(ulong key)
@@ -64,7 +80,14 @@ namespace GnuClay.Engine.LogicalStorage.InternalStorage
 
         private void RemovePartIndex(RulePart rulePart)
         {
-            throw new NotImplementedException();
+            var targetKeys = mRelationPartsDict[rulePart];
+            mRelationPartsDict.Remove(rulePart);
+
+            foreach(var key in targetKeys)
+            {
+                var tmpList = mRelationPartsIndex[key];
+                tmpList.Remove(rulePart);
+            }
         }
 
         public void AddEntity(ulong key)
@@ -140,14 +163,13 @@ namespace GnuClay.Engine.LogicalStorage.InternalStorage
             {
                 RemovePartIndex(part_2);
             }
-
-            throw new NotImplementedException();
         }
 
         public object Save()
         {
             var tmpData = new Data();
             tmpData.mRelationPartsIndex = mRelationPartsIndex;
+            tmpData.mRelationPartsDict = mRelationPartsDict;
             tmpData.mRulesAndFactsDict = mRulesAndFactsDict;
             tmpData.mLongHasheCodeRulesAndFactsDict = mLongHasheCodeRulesAndFactsDict;
             tmpData.mEntitiesList = mEntitiesList;
@@ -158,6 +180,7 @@ namespace GnuClay.Engine.LogicalStorage.InternalStorage
         {
             var tmpData = (Data)value;
             mRelationPartsIndex = tmpData.mRelationPartsIndex;
+            mRelationPartsDict = tmpData.mRelationPartsDict;
             mRulesAndFactsDict = tmpData.mRulesAndFactsDict;
             mLongHasheCodeRulesAndFactsDict = tmpData.mLongHasheCodeRulesAndFactsDict;
             mEntitiesList = tmpData.mEntitiesList;
@@ -166,6 +189,7 @@ namespace GnuClay.Engine.LogicalStorage.InternalStorage
         public void Clear()
         {
             mRelationPartsIndex = new Dictionary<ulong, List<RulePart>>();
+            mRelationPartsDict = new Dictionary<RulePart, List<ulong>>();
             mRulesAndFactsDict = new Dictionary<ulong, RuleInstance>();
             mLongHasheCodeRulesAndFactsDict = new Dictionary<ulong, List<RuleInstance>>();
             mEntitiesList = new List<ulong>();
