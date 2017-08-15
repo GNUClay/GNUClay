f7f6752ed3831c456f4fd787fc0c06ceaff2b9c6
Sergiy Tolkachov <metatypeman@gmail.com>
2017-08-15 17:06:25 +0300

    #5

--- GnuClay.Engine/StandardLibrary/SupportingMachines/BooleanValue.cs
+++ GnuClay.Engine/StandardLibrary/SupportingMachines/BooleanValue.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GnuClay.Engine.InternalCommonData;
+using GnuClay.Engine.ScriptExecutor.CommonData;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +8,23 @@ using System.Threading.Tasks;
 
 namespace GnuClay.Engine.StandardLibrary.SupportingMachines
 {
-    public class BooleanValue
+    public class BooleanValue : BaseValueType
     {
-        public BooleanValue()
+        public BooleanValue(ulong typeKey, object value)
+            : base(typeKey, value)
         {
-  
+            OriginalValue = (bool)(Value);
+        }
+
+        public bool OriginalValue = false;
+
+        /// <summary>
+        /// Converts the value of this instance to its equivalent string representation. Overrides (Object.ToString)
+        /// </summary>
+        /// <returns>The string representation of this instance.</returns>
+        public override string ToString()
+        {
+            return $"BooleanValue {nameof(TypeKey)} = {TypeKey}; {nameof(Value)} = {Value}";
         }
     }
 }
