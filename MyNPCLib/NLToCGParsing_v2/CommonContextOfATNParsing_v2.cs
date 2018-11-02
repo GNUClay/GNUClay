d8c63c3001edeedca08375b23e071e4428f2601b
Sergiy Tolkachov <metatypeman@gmail.com>
2018-11-02 17:55:28 +0200

    snapshot

--- MyNPCLib/NLToCGParsing_v2/CommonContextOfATNParsing_v2.cs
+++ MyNPCLib/NLToCGParsing_v2/CommonContextOfATNParsing_v2.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyNPCLib.NLToCGParsing.PhraseTree;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,23 @@ namespace MyNPCLib.NLToCGParsing_v2
 {
     public class CommonContextOfATNParsing_v2
     {
+        private IList<Sentence> mSentencesList = new List<Sentence>();
+
+        public void AddSentence(Sentence sentence)
+        {
+#if DEBUG
+            //LogInstance.Log($"sentence = {sentence}");
+#endif
+
+            mSentencesList.Add(sentence);
+        }
+
+        public IList<Sentence> SentencesList
+        {
+            get
+            {
+                return mSentencesList;
+            }
+        }
     }
 }
--- MyNPCLib/NLToCGParsing_v2/ContextOfATNParsing_v2.cs
+++ MyNPCLib/NLToCGParsing_v2/ContextOfATNParsing_v2.cs
@@ -3,11 +3,12 @@ using MyNPCLib.NLToCGParsing.PhraseTree;
 using MyNPCLib.SimpleWordsDict;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MyNPCLib.NLToCGParsing_v2
 {
-    public class ContextOfATNParsing_v2
+    public class ContextOfATNParsing_v2: IObjectToString
     {
         public ContextOfATNParsing_v2(string text, IWordsDict wordsDict, CommonContextOfATNParsing_v2 commonContext)
         {
@@ -22,6 +23,38 @@ namespace MyNPCLib.NLToCGParsing_v2
         private CommonContextOfATNParsing_v2 CommonContext;
         private ATNExtendedLexer mATNExtendedLexer;
         public Sentence Sentence { get; set; }
+        public Stack<BaseNounLikePhrase> OperativeNounPhrasesStack = new Stack<BaseNounLikePhrase>();
+        public void AddNounLikePhrase(BaseNounLikePhrase nounPhrase)
+        {
+            OperativeNounPhrasesStack.Push(nounPhrase);
+        }
+
+        public BaseNounLikePhrase PeekCurrentNounPhrase()
+        {
+            if (OperativeNounPhrasesStack.Count == 0)
+            {
+                return null;
+            }
+
+            return OperativeNounPhrasesStack.Peek();
+        }
+
+        public Stack<VerbPhrase> OperativeVerbPhraseStack = new Stack<VerbPhrase>();
+
+        public void AddVerbPhrase(VerbPhrase verbPhrase)
+        {
+            OperativeVerbPhraseStack.Push(verbPhrase);
+        }
+
+        public VerbPhrase PeekCurrentVerbPhrase()
+        {
+            if (OperativeVerbPhraseStack.Count == 0)
+            {
+                return null;
+            }
+
+            return OperativeVerbPhraseStack.Peek();
+        }
 
         public ContextOfATNParsing_v2 Fork()
         {
@@ -35,6 +68,67 @@ namespace MyNPCLib.NLToCGParsing_v2
         public IList<ATNExtendedToken> GetСlusterOfExtendedTokens()
         {
             return mATNExtendedLexer.GetСlusterOfExtendedTokens();
+        }
+
+        public void PutSentenceToResult()
+        {
+            CommonContext.AddSentence(Sentence);
+        }
+
+        public override string ToString()
+        {
+            return ToString(0u);
+        }
+
+        public string ToString(uint n)
+        {
+            return this.GetDefaultToStringInformation(n);
+        }
+
+        public string PropertiesToSting(uint n)
+        {
+            var spaces = StringHelper.Spaces(n);
+            var nextN = n + 4;
+            var sb = new StringBuilder();
+            if (Sentence == null)
+            {
+                sb.AppendLine($"{spaces}{nameof(Sentence)} = null");
+            }
+            else
+            {
+                sb.AppendLine($"{spaces}Begin {nameof(Sentence)}");
+                sb.Append(Sentence.ToString(nextN));
+                sb.AppendLine($"{spaces}End {nameof(Sentence)}");
+            }
+            if (OperativeNounPhrasesStack == null)
+            {
+                sb.AppendLine($"{spaces}{nameof(OperativeNounPhrasesStack)} = null");
+            }
+            else
+            {
+                sb.AppendLine($"{spaces}Begin {nameof(OperativeNounPhrasesStack)}");
+                var operativeNounPhrasesList = OperativeNounPhrasesStack.ToList();
+                foreach (var operativeNounPhrase in operativeNounPhrasesList)
+                {
+                    sb.Append(operativeNounPhrase.ToString(nextN));
+                }
+                sb.AppendLine($"{spaces}End {nameof(OperativeNounPhrasesStack)}");
+            }
+            if (OperativeVerbPhraseStack == null)
+            {
+                sb.AppendLine($"{spaces}{nameof(OperativeVerbPhraseStack)} = null");
+            }
+            else
+            {
+                sb.AppendLine($"{spaces}Begin {nameof(OperativeVerbPhraseStack)}");
+                var operativeVerbPhraseList = OperativeVerbPhraseStack.ToList();
+                foreach (var operativeVerbPhrase in OperativeVerbPhraseStack)
+                {
+                    sb.Append(operativeVerbPhrase.ToString(nextN));
+                }
+                sb.AppendLine($"{spaces}End {nameof(OperativeVerbPhraseStack)}");
+            }
+            return sb.ToString();
         }
     }
 }
