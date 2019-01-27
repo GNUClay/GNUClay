a7e77da1d48467594ffed1589af279448784a306
Sergiy Tolkachov <metatypeman@gmail.com>
2019-01-27 19:42:55 +0200

    snapshot

--- GnuClay/Engine.cs
+++ GnuClay/Engine.cs
@@ -10,6 +10,11 @@ namespace GnuClay
     /// </summary>
     public class Engine
     {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="appFileName">File name of application of the NPC.</param>
+        /// <param name="host">Reference to instance of host.</param>
         public Engine(string appFileName, IHost host)
         {
         }
--- GnuClay/IHost.cs
+++ GnuClay/IHost.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace GnuClay
 {
+    /// <summary>
+    /// Represent host of the NPC.
+    /// </summary>
     public interface IHost
     {
     }
